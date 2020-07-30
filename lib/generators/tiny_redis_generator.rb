0ee5d4dd936ed231a073b994a0449694e5afcc35
Pavel Zaitsev <pavel@arslogi.ca>
2020-07-30 17:13:58 -0400

    improved initializer added vars
    RAILS_MAX_CONCURRENCY - number of redis connection will be created
    REDIS_TIMEOUT - timeout for how long redis client will wait
    REDIS_URL - url for the redis sever.
              - note that for :test env you also need to set it if you don't want to use default value

--- lib/generators/tiny_redis_generator.rb
+++ lib/generators/tiny_redis_generator.rb
@@ -1,11 +1,22 @@
 class TinyRedisGenerator < Rails::Generators::Base
   def create_tiny_redis_file
     create_file "config/initializers/tiny-redis.rb", <<EOD
+concurrency = ENV.fetch("RAILS_MAX_CONCURRENCY") {5}
+timeout = ENV.fetch("REDIS_TIMEOUT") {1}
+
+url = ENV.fetch("REDIS_URL"){
+  if Rails.env.test?
+    "redis://127.0.0.1:6379/2"
+  else
+    "redis://127.0.0.1:6379/1"
+  end
+}
+
 if Rails.env.test?
-  $redis = ConnectionPool::Wrapper.new(size: 5, timeout: 3) { Redis.new(url: 'redis://127.0.0.1:6379/2') }
+  $redis = ConnectionPool::Wrapper.new(size: concurrency, timeout: timeout) { Redis.new(url: url )}
   $redis.flushall
 else
-  $redis = ConnectionPool::Wrapper.new(size: 5, timeout: 3) { Redis.new(url: 'redis://127.0.0.1:6379/1') }
+  $redis = ConnectionPool::Wrapper.new(size: concurrency, timeout: timeout) { Redis.new(url: url )}
 end
 EOD
   end
